f20c97eb4c91087420207bdc83f15e0d81078e9b
fseeyounala <1440233578@qq.com>
2021-08-13 16:51:50 +0800

    第二次提交之前更新

--- src/main/java/com/jt/test/HelloGit.java
+++ src/main/java/com/jt/test/HelloGit.java
@@ -8,8 +8,8 @@ public static void main(String[] args) {
 	System.out.println("主干添加");
 	System.out.println("分支添加");
 	System.out.println("测试commit");
-
 	System.out.println("测试commit1");
+	System.out.println("第二次提交之前更新");
 
 
 
